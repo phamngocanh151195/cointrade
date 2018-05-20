3c55973c01de81facebba09c40a13ffb83b256e4
phamngocanh151195 <0ongocanh0ovt12@gmail.com>
2018-05-20 16:39:22 +0700

    hi1234

--- test.php
+++ test.php
@@ -1,67 +1,77 @@
+
 <? php
 
-
-	$__page = str_replace('http://45.119.82.176/', '', $_SERVER['REQUEST_URI']);
-	header("Access-Control-Allow-Origin: *");
-	header('Access-Control-Allow-Credentials: true');
-	header('Access-Control-Allow-Headers: Content-Type');
-	//header('Access-Control-Max-Age: 86400');// cache for 1 day
-
-	if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-			header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-			header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-		exit(0);
-	}
-
-	if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST)) {
-	    $_POST = json_decode(file_get_contents('php://input'), true);
-	}
-?>
-<!DOCTYPE html>
 <html lang="en">
 <head>
-        <title>Login V1</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login V1</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+    function submitLoginForm() {
+        console.log('hello');
+    $.ajax({
+                url: 'http://45.119.82.176:8000/login/user/',
+                type: "post",
+                data: $('#login').serialize(),
+                success: function (response) {
+                    console.log(response);
+                    if (("token" in response) == false) {
+                    } else {
+                        __token = response.token;
+                        localStorage.setItem("token" , __token);
+                        localStorage.setItem("login_time" , Math.floor(Date.now() / 1000));
+                        console.log(__token);
+                        window.location.href = 'https://google.com/';
+                    }
+                },
+                error: function (a, b, c) {
+                   alert(JSON.stringify(a));
+                }
+            });
+    }
+    </script>
 </head>
 <body>
-        <form id="login" onsubmit = "return submitLoginForm()" method = "POST">
-            <input id="email1" type="text" name="username" value="tuan">
-            <input id="password1" type="password" name="password" placeholder="Password" value="123456">
-            <input type="submit" name="submit"  value='Ckick em di'>
-        </form>
+    <div class="col-lg-2"></div>
 
-        <script src="js/jquery.min.js"></script>
+<div class="col-lg-8 no-padding" style="margin:15px 0">
+    <h4 class="with-border page-title show">Đăng nhập</h4>
+    <form id="login" onsubmit="return submitLoginForm()" method="POST" style="margin-top:15px">
+        <div class="form-group">
+            <div class="col-lg-3 control-label">Tên đăng nhập </div>
+            <div class="col-lg-9">
+                <input class="form-control" type="text" name="username" value="tuan"/>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="form-group">
+            <div class="col-lg-3 control-label">Mật khẩu </div>
+            <div class="col-lg-9">
+                <input class="form-control" type="password" name="password" value="123456"/>
+            </div>
+            <div class="clearfix"></div>
+        </div>
 
-        <script>
-                function submitLoginForm() {
-	            console.log('hello');
-	            //alert('<?php echo $_SERVER['REQUEST_URI']; ?>');
-	            $.ajax({
-	                url: 'http://45.119.82.176:8000/login/user/',
-	                type: "post",
-	                data: $('#login').serialize(),
-	                contentType: 'application/json',
-	                success: function (response) {
-	                    console.log(response);
-	                    if (("token" in response) == false) {
-	                    } else {
-	                        __token = response.token;
-	                        localStorage.setItem("token" , __token);
-	                        localStorage.setItem("login_time" , Math.floor(Date.now() / 1000));
-	                        console.log(__token);
-	                        window.location.href = 'https://google.com/';
-						}
-	                },
-	                error: function (a, b, c) {
-	                   alert(JSON.stringify(a));
-	                }
-	             });
-            }
-        </script>
+        <div class="form-group">
+            <div class="col-lg-3 control-label">Đăng nhập với tư cách</div>
+            <div class="col-lg-3">
+                <label><input name="type" type="radio" checked value="mod"> Mod</label>
+            </div>
+            <div class="col-lg-3">
+                <label><input name="type" type="radio" value="smod"> Smod</label>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+
+        <div class="add-form-submit form-one-button center">
+            <input type="submit" class="btn" value="Đăng nhập"/>
+        </div>
+    </form>
+</div>
+
+<div class="col-lg-2"></div>
+<div class="clearfix"></div>
 </body>
 </html>
+
